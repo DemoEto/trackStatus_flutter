2d42d6e4631e55bfcbf4495c7a19a8146ed9b148
DemoEto <paintcillssz@gmail.com>
2025-07-04 12:22:58 +0700

    U:Textbox register

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -66,8 +66,6 @@ class _LoginPageState extends State<LoginPage> {
     }
     try {
       await AuthService().register(
-        text: _controllerFname,
-        text: _controllerLname,
         email: _controllerEmail.text,
         password: _controllerPassword.text,
       );
