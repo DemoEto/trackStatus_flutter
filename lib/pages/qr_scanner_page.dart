f5eb5838c8f6aede2abdb961cee1874c02c74f3f
DemoEto <paintcillssz@gmail.com>
2025-08-18 03:38:50 +0700

    M: scan route with white page

--- lib/pages/qr_scanner_page.dart
+++ lib/pages/qr_scanner_page.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class QRScannerPage extends StatelessWidget {
-  const QRScannerPage({super.key});
+import '../routes/app_route.dart';
+
+class QrScannerPage extends StatefulWidget {
+  const QrScannerPage({super.key});
+
+  @override
+  State<QrScannerPage> createState() => _QrScannerPageState();
+}
+
+class _QrScannerPageState extends State<QrScannerPage> {
+  final MobileScannerController _controller = MobileScannerController();
+  bool _scanned = false; // กันซ้ำ
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('สแกน QR Code'),
-      ),
       body: MobileScanner(
+        controller: _controller,
         onDetect: (capture) {
+          if (_scanned) return; // กันซ้ำ
+          _scanned = true;
+
           final List<Barcode> barcodes = capture.barcodes;
           for (final barcode in barcodes) {
             final String? code = barcode.rawValue;
             if (code != null) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('พบ QR Code: $code')),
-              );
+              // หยุดกล้องทันที
+              _controller.stop();
+
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('พบ QR Code: $code')));
+
+              // ไปหน้าใหม่
+              context.push(code);
             }
           }
         },
--- lib/routes/route_config.dart
+++ lib/routes/route_config.dart
@@ -51,7 +51,7 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: AppRoutes.qrScan,
-      builder: (context, state) => const QRScannerPage(),
+      builder: (context, state) => const QrScannerPage(),
     ),
   ],
 );
