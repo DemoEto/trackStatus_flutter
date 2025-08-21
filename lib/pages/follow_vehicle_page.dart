5d056b8f77fa9c33a910aeaf94cb129192245a74
DemoEto <paintcillssz@gmail.com>
2025-08-22 00:14:00 +0700

    U:image for status

--- lib/pages/follow_vehicle_page.dart
+++ lib/pages/follow_vehicle_page.dart
@@ -8,23 +8,46 @@ class FollowVehiclePage extends StatefulWidget {
 }
 
 class _FollowVehiclePageState extends State<FollowVehiclePage> {
+  String status = "";
+  Widget _takeStudent() {
+    return Column(
+      children: [
+        Center(child: Image.asset('assets/images/school-bus.gif')),
+        Padding(
+          padding: EdgeInsetsGeometry.directional(top: 10),
+          child: Text('กำลังรับนักเรียนไปโรงเรียน', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    );
+  }
+  Widget _aliveSchool() {
+    return Column(
+      children: [
+        Center(child: Image.asset('assets/images/aliveschool.gif')),
+        Padding(
+          padding: EdgeInsetsGeometry.directional(top: 10),
+          child: Text('กำลังรับนักเรียนไปโรงเรียน', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    );
+  }
+  Widget _goSchool() {
+    return Column(
+      children: [
+        Center(child: Image.asset('assets/images/goschool.gif')),
+        Padding(
+          padding: EdgeInsetsGeometry.directional(top: 10),
+          child: Text('กำลังรับนักเรียนไปโรงเรียน', style: TextStyle(fontSize: 20)),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Detec Vehicle'),
-      ),
-      body: Column(
-        children: [
-          Center(
-            child: Image.asset('assets/images/avavav.jpg'),
-          ),
-          Padding(
-            padding: EdgeInsetsGeometry.directional(top: 10),
-            child: Text('Vehicle Status: '),
-          )
-        ],
-      ),
+      appBar: AppBar(title: Text('Detect Vehicle')),
+      body: Placeholder()
     );
   }
 }
